0b1b80bf10e90243424b9a7d730f5116d58c7962
Bob Gunion <bob.gunion@gmail.com>
2014-11-01 19:38:54 -0700

    Added logout.php

---
+++ src/logout.php
@@ -0,0 +1,31 @@
+<?php
+
+/* 
+ * Copyright (C) 2014 rfgunion.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+
+require_once('auth.php');
+require_once('classes/User.php');
+
+if (getAccessLevel() > User::NO_ACCESS) {
+	$user = new User($_SESSION['bycuserid']);
+	unset($_SESSION['bycuserid']);
+	$user->tstamp = '';
+	$user->save();
+}
+header('Location: index.php');
