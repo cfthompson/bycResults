90300bc0971a97bb1dc3afac3f4e1d6614ca23af
Bob Gunion <bob.gunion@gmail.com>
2014-11-19 08:21:26 -0800

    Added radio buttons for normal/cheatsheet views

---
+++ src/js/race.js
@@ -0,0 +1,58 @@
+/* 
+ * Copyright (C) 2014 rfgunion.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+
+/* querystring function adapted from Quentin's answer at
+ * http://stackoverflow.com/questions/979975/how-to-get-the-value-from-url-parameter
+ */
+var querystring = function () {
+  // This function is anonymous, is executed immediately and 
+  // the return value is assigned to QueryString!
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    	// If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = pair[1];
+    	// If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]], pair[1] ];
+      query_string[pair[0]] = arr;
+    	// If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(pair[1]);
+    }
+  } 
+    return query_string;
+} ();
+
+function onSelectView() {
+	var raceid = parseInt(querystring.id);
+	var id = $(this).attr("id");
+	if (id === "view_normal") {
+		window.location.replace("race.php?id="+raceid);
+	} else {
+		window.location.replace("cheatsheet.php?id="+raceid);
+	}
+}
+
+$(function() {
+	$("input:radio.view_select").change(onSelectView);
+});
