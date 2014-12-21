39398e00d893aaf2bc9cb31fd9a5214d0605362b
Bob Gunion <bob.gunion@gmail.com>
2014-12-21 08:48:39 -0800

    Added filters and accessRules methods

--- src/protected/controllers/RaceController.php
+++ src/protected/controllers/RaceController.php
@@ -2,6 +2,39 @@
 
 class RaceController extends Controller
 {
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+			'postOnly + delete', // we only allow deletion via POST request
+		);
+	}
+
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+		return array(
+			array('allow',  // allow all users to perform 'index' action
+				'actions'=>array('index','view','divisions'),
+				'users'=>array('*'),
+			),
+			array('allow', // allow authenticated user to perform CRUD actions
+				'actions'=>array('create','edit','entries','delete'),
+				'users'=>array('@'),
+			),
+			array('deny',  // deny all users
+				'users'=>array('*'),
+			),
+		);
+	}
+
 	public function actionDelete()
 	{
 		$this->render('delete');
