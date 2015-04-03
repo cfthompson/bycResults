30101d796033c12388fe956cbfef6335c58d9d7c
Bob Gunion <bob.gunion@gmail.com>
2015-04-03 13:17:50 -0700

    Check for logged-in user when displaying "Add a New Series" link

--- src/protected/views/site/index.php
+++ src/protected/views/site/index.php
@@ -8,9 +8,11 @@ $this->pageTitle=Yii::app()->name;
 
 <h1>Berkeley Yacht Club Friday Night and Sunday Chowder Series</h1>
 
+<?php if (!Yii::app()->user->isGuest) { ?>
 <a href="<?php echo CHtml::normalizeUrl(array('/series/create')); ?>">Add a New Series</a>
 <br/>
 <br/>
+<?php } ?>
 
 <?php $this->widget('CTreeView', array(
 	'data'=>$data,
