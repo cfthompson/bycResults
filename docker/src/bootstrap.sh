6212fb7055eab459189e4a924684c4daa55de6bc
Colin Thompson <colin.forrest.thompson@gmail.com>
2024-09-15 18:56:51 -0700

    revoking mysql user bash shell after DB is set up

--- docker/src/bootstrap.sh
+++ docker/src/bootstrap.sh
@@ -3,4 +3,5 @@
 service mariadb start
 sed -i 's/MariaDB.*\/false/MariaDB:\/nonexistent:\/bin\/bash/' /etc/passwd
 su -c/dbsetup.sh mysql
+sed -i 's/MariaDB.*\/bash/MariaDB:\/nonexistent:\/bin\/false/' /etc/passwd
 apache2-foreground
