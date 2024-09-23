39d94e566467756a1edfa39d5d9dca5a8c24b17d
Colin Thompson <colin.forrest.thompson@gmail.com>
2024-09-23 14:11:24 -0700

    cleaned up localtime and fiddled with cron for DB backups

--- docker/src/backupDB.sh
+++ docker/src/backupDB.sh
@@ -3,6 +3,6 @@
 DATE=`date '+%Y-%m-%d'`
 cd /tmp/mysqlrepo
 git pull
-mysqldump -ubyc -pbyc@1939 berkele6_results > /tmp/mysqlrepo/berkele6_results.sql
+mysqldump -h127.0.0.1 -ubyc -pbyc@1939 berkele6_results > /tmp/mysqlrepo/berkele6_results.sql
 git commit -a -m "$DATE beercan results DB backup"
 git push
--- docker/src/bootstrap.sh
+++ docker/src/bootstrap.sh
@@ -4,7 +4,10 @@ REPOURL=https://github.com/cfthompson/bycBeerCanDB.git
 
 git clone $REPOURL /tmp/mysqlrepo >> /dbsetup.log 2>&1
 chown -R mysql:mysql /tmp/mysqlrepo >> /dbsetup.log 2>&1
+cd /tmp/mysqlrepo
+git config --global safe.directory '*'
 
+service cron start
 service mariadb start
 sed -i 's/MariaDB.*\/false/MariaDB:\/nonexistent:\/bin\/bash/' /etc/passwd
 su -c/dbsetup.sh mysql
